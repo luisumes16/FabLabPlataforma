5ebf244bbf714ed1848ee19a39b74ae6e8387fa2
lisyGuz123 <142834050+lisyGuz123@users.noreply.github.com>
2023-09-05 09:19:51 -0600

    Add files via upload

--- recepcion.js
+++ recepcion.js
@@ -33,26 +33,36 @@ const querySnapshot = await getDocs(q);
 querySnapshot.forEach((doc) => {
   // doc.data() is never undefined for query doc snapshots
   console.log(doc.id, " => ", doc.data());
-  nuevoDiv(doc.data().nnombre,doc.data().descripcion,doc.data().material)
+  nuevoDiv(doc.data().nnombre,doc.data().telefo,doc.data().descripcion,doc.data().medidas,doc.data().cantidad,doc.data().material)
 });
 }
 
 
 
-function nuevoDiv(nombre,descripcion,material){
+function nuevoDiv(nombre,telefono,descripcion,medidas,cantidad,material){
   var divdatos=document.getElementById("ii")
   var div=document.createElement("div")
   div.className="card mt-4 p-3"
  var h=document.createElement("h3")
- h.textContent=nombre
+ h.textContent="Nombre del cliente: "+nombre
  var parrafo=document.createElement("p")
- parrafo.textContent=descripcion
+ parrafo.textContent="Tel: "+telefono
  var parrafo2=document.createElement("p") 
- parrafo2.textContent=material
+ parrafo2.textContent="Decripción del producto: "+descripcion
+ var parrafo3=document.createElement("p")
+ parrafo3.textContent="Medidas del producto: "+medidas
+ var parrafo4=document.createElement("p")
+ parrafo4.textContent="cantidad: "+cantidad
+ var parrafo5=document.createElement("p")
+ parrafo5.textContent="material: " +material
+
  divdatos.appendChild(div)
  div.appendChild(h)
  div.appendChild(parrafo)
  div.appendChild(parrafo2)
+ div.appendChild(parrafo3)
+ div.appendChild(parrafo4)
+ div.appendChild(parrafo5)
  
  
 }
