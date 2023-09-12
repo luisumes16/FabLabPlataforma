27d18f6bb39f70153302d78760201b9b021ba58d
luisumes16 <123779843+luisumes16@users.noreply.github.com>
2023-09-12 16:07:59 -0600

    Update cotizacion.js
    Nn

--- cotizacion.js
+++ cotizacion.js
@@ -1,47 +1,102 @@
 
+
 document.getElementById('divp2').hidden = true
 
-console.log('hola mundo')
-let radioBtns = document.querySelectorAll("input[name='familia']")
+document.getElementById('selectCalculo').addEventListener('change', () => {
+    var valor = document.getElementById('selectCalculo').value
+    if (valor == '1') {
 
-let findSelectd = ()=> {
-    let selected = document.querySelector("input[name='fruit']:checked").value
-    console.log(selected)
-}
+        document.getElementById('divp2').hidden = true
+    } else if (valor == '2') {
 
-
-
-document.getElementById('btncalcular').addEventListener('click',()=>{
-    let elementoActivo = document.querySelector('input[name="status"]:checked');
-    if(elementoActivo) {
-        alert(elementoActivo.value);
-    } else {
-        alert('No hay ninún elemento activo');
+        document.getElementById('divp2').hidden = false
     }
+})
+
+
+
+
+
+document.getElementById('btncalcular').addEventListener('click', () => {
+    var valor = document.getElementById('selectCalculo').value
+    var lprecio = document.getElementById('lprecio')
+    var lprecioM = document.getElementById('lprecioM')
+    var preciot = 0
+    if (valor == '1'){
+        preciot = onlyMaterial()
+    }else {
+        preciot = onlyMaterial()
+        preciot += tiempoLaser()
+    }
+    
+    
+    lprecio.textContent = "Precio: Q." + preciot.toFixed(2)
+    lprecioM.textContent = "Precio: Q." + preciot.toFixed(2)
+})
+
+
+function onlyMaterial() {
+
+
+//ACRILICO NEGRO
+    //var precioplancha = 475
+
+//ACRILICO
+    //var precioplancha = 450
+   //MDF
+   //var precioplancha = 165
+   //MADERA 1.2 por 2.4
+    var precioplancha = 600
+    //
+    var divisiones = 5
+    var pdiv = precioplancha/divisiones
+    var cm21 = 90*60  
+
     var ancho = document.getElementById('inputancho').value
     var largo = document.getElementById('inputlargo').value
     var cantidad = document.getElementById('inputcantidad').value
-    var lprecio = document.getElementById('lprecio')
-    var lprecioM = document.getElementById('lprecioM')
+
     var lpreciou = document.getElementById('lpreciou')
     var lcm = document.getElementById('lcm')
     var cm = ancho * largo;
-    lcm.textContent = "Cm2: "+ cm+"cm."
-    var preciocm = 0.00556
-    var preciou = preciocm*cm
-    lpreciou.textContent = "Precio: Q."+preciou.toFixed(2)
+    lcm.textContent = "Cm2: " + cm + "cm."
+    var preciocm = 0.0208383    
+    //var preciocm = pdiv/cm21
+    console.log(preciocm)
+    var preciou = preciocm * cm
+    lpreciou.textContent = "Precio: Q." + preciou.toFixed(2)
 
-    if(preciou > 25){
-        var preciot = (preciou*cantidad)*1.5
+    if (preciou > 25) {
+        var preciot = (preciou * cantidad) * 1.5
+    } else {
+        var preciot = (preciou * cantidad) * 2
+    }
+
+    return preciot
+
+}
+
+function tiempoLaser() {
+    var tiempo = document.getElementById('inputtiempo').value
+    if (tiempo == " "){
+        return 0
     }else {
-        var preciot = (preciou*cantidad)*2
+        var preciotiempo = tiempo * 3
+        console.log(preciotiempo)
+        return preciotiempo
     }
     
+}
+function ganancia() {
+    var tiempo = document.getElementById('inputtiempo').value
+    if (tiempo == " "){
+        return 0
+    }else {
+        var preciotiempo = tiempo * 4
+        console.log(preciotiempo)
+        return preciotiempo
+    }
+    
+}
 
-    lprecio.textContent = "Precio: Q."+preciot.toFixed(2)
-    lprecioM.textContent = "Precio: Q."+preciot.toFixed(2)
-
-
-
-})
 
